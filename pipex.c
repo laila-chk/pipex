06beb392d266a738800336a221aea2581fa4bd14
Laila Chokri <lchokri@e1r9p1.1337.ma>
2022-06-14 19:33:21 +0100

    init

--- pipex.c
+++ pipex.c
@@ -6,17 +6,31 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 18:30:12 by lchokri           #+#    #+#             */
-/*   Updated: 2022/06/14 19:13:28 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/06/14 19:33:16 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int main(int ac, char **av)
+int	get_path(char **envp, char *cmd)
 {
+	//split those paths && search for the cmd inside them
+}
+
+int main(int ac, char **av, char **envp)
+{
+	int		fd;
+	int		chld_pid;
+
 	if (ac == 5)
 	{
-		// do things
+		fd = open(av[1], O_RDWR);
+		chld_pid = fork();	
+		if (chld_pid == 0)
+		{
+			execve("", );
+		}
+		close(fd);
 	}
 	else
 		printf("you should enter 4 argument beside the executable instead of %d", ac - 1);
